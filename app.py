4b7a57ddf3548b57153ba903fa9f901c15a8a2b3
jggoyaljayati <jggoyaljayati@gmail.com>
2024-02-15 23:43:27 -0700

    first commit

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, Response, request
+import pandas as pd
+
+app = Flask(__name__)
+data = pd.read_csv("Classification Results on Face Dataset (1000 images).csv")
+
+@app.route("/", methods=['POST'])
+def home():
+    try:
+        file = request.files['inputFile']
+        filename = file.filename[:-4]
+        result = data[data['Image'] == filename].Results.item()
+
+        return Response(("{}:{}").format(filename, result), status=200, mimetype='application/json')
+
+    except Exception as e:
+        return Response(f"Error: {str(e)}", status=500, mimetype='application/json')
+
+if __name__ == "__main__":
+    app.run(debug=True)
