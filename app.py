e03478984a1a264468c0278451ea54cd5a367e25
jggoyaljayati <jggoyaljayati@gmail.com>
2024-03-16 23:54:14 -0700

    jesfdgjyef

--- app.py
+++ app.py
@@ -1,20 +1,48 @@
 from flask import Flask, Response, request
 import pandas as pd
+import boto3
+import uuid
+# import time
 
+s3 = boto3.client('s3', 'us-east-1')
+sqs = boto3.client('sqs', 'us-east-1')
+request_queue_url = 'https://sqs.us-east-1.amazonaws.com/654654147126/1227629236-req-queue'
+response_queue_url = 'https://sqs.us-east-1.amazonaws.com/654654147126/1227629236-resp-queue'
+input_bucket = '1227629236-in-bucket'
 app = Flask(__name__)
-data = pd.read_csv("Classification Results on Face Dataset (1000 images).csv")
+
+# response_queue = queue.Queue()
 
 @app.route("/", methods=['POST'])
-def home():
-    try:
-        file = request.files['inputFile']
-        filename = file.filename[:-4]
-        result = data[data['Image'] == filename].Results.item()
+def send_request():
+    file = request.files['inputFile']
+    print("FILE: ",file.filename)
+    s3.upload_fileobj(Fileobj=file, Bucket=input_bucket, Key=file.filename)
+    request_id = str(uuid.uuid4())
+    sqs.send_message(QueueUrl=request_queue_url,MessageBody=(file.filename), MessageAttributes={'RequestId': {'StringValue': request_id, 'DataType': 'String'}})
+    # result =  recieve_response(request_id)
+    # return Response(result, status=200)
 
-        return Response(("{}:{}").format(filename, result), status=200, mimetype='application/json')
+# def recieve_response(request_id):
+    # print("HI")
+    while (True):
+        response = sqs.receive_message(QueueUrl=response_queue_url,VisibilityTimeout=1,MaxNumberOfMessages=1,WaitTimeSeconds=10,MessageAttributeNames=['All'])
+        messages = response.get('Messages', [])
+        if messages:
+            for message in messages:
+                if message.get('MessageAttributes', {}).get('RequestId', {}).get('StringValue') == request_id:
+                    result = message['Body']
+                    # print("message Found")
+                    # print(message['Body'])
+                    response = sqs.delete_message(QueueUrl=response_queue_url,ReceiptHandle=message['ReceiptHandle'])
+                    # print(response)
+                    return Response(result, status=200)
 
-    except Exception as e:
-        return Response(f"Error: {str(e)}", status=400, mimetype='application/json')
+# def test():
+#     response = sqs.send_message(QueueUrl=request_queue_url,MessageBody=("test_000.jpg"))
+#     print(response)
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0")
+    # test()
+    app.run(debug=True, host="0.0.0.0", port="8000")
+    
---
+++ scaling.py
@@ -0,0 +1,48 @@
+import time
+import boto3
+
+request_queue_url = 'https://sqs.us-east-1.amazonaws.com/654654147126/1227629236-req-queue'
+
+sqs = boto3.client('sqs', region_name="us-east-1")
+ec2 = boto3.client('ec2', region_name="us-east-1")
+
+def groupInstances(appTierInstances):
+    groupedInstances = {'stopping': [], 'running': [],  'stopped': [], 'pending': []}
+    for x in appTierInstances['Reservations']:
+        for i in x['Instances']:
+            groupedInstances[i['State']['Name']].append(i['InstanceId'])
+
+    return groupedInstances
+
+def getMessageCount():
+    attr = sqs.get_queue_attributes(QueueUrl=request_queue_url,AttributeNames=['ApproximateNumberOfMessages'])
+    return int(attr['Attributes']['ApproximateNumberOfMessages'])
+
+def getActiveInstances(groupedInstances):
+    return len(groupedInstances['running']) + len(groupedInstances['pending'])
+
+def scale():
+    while True:
+        try:
+            appTierInstances = ec2.describe_instances(Filters=[{'Name': 'tag:Name', 'Values': ["app-tier-instance*"]},{'Name': 'instance-state-name', 'Values': ['running', 'stopped', 'pending', 'stopping']}])
+            groupedInstances = groupInstances(appTierInstances)
+            activeInstances = getActiveInstances(groupedInstances)
+            if activeInstances:
+                print(activeInstances)
+            messages = min(20, getMessageCount())
+            if messages - activeInstances > 0:
+                if groupedInstances['stopped']:
+                    ec2.start_instances(InstanceIds=groupedInstances['stopped'][:(messages - activeInstances)])
+                    # time.sleep(10)
+            elif messages - activeInstances < 0:
+                if groupedInstances['running']:
+                    ec2.stop_instances(InstanceIds=groupedInstances['running'][:abs(messages - activeInstances)])
+                    # time.sleep(10)
+            # else:
+            #     time.sleep(5)
+        except Exception as e:
+            print(e)
+
+
+if __name__ == "__main__":
+    scale()
